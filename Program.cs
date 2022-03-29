5cb05455f1a697c60a63d7100e22ad67d4cbb5d1
Carmel <102567251+Carmel132@users.noreply.github.com>
2022-03-28 22:17:32 -0400

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace WordleSolver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WordleClass wc = new WordleClass();
+            wc.Run();
+            Console.ReadLine();
+        }
+    }
+}
